b3d2905cfbe4f65f7bb266ea03d171ab7021714b
Ronak Vyas <ronvyas1@gmail.com>
2020-09-19 11:19:10 -0700

    Update canvas.py

--- canvas.py
+++ canvas.py
@@ -1,29 +1,52 @@
 import os
+import sys
 
 # Import Canvas
 from canvasapi import Canvas
+
 
 # Canvas API
 API_URL = "https://canvas.instructure.com/"
 API_KEY = "7~a0WZUQK2LiJBRU9GBGIIb99it5rA8MMB8Jt5ANEv0C2ilgi0hgwsztOVPebnT0DD"
 
 # Initialize Canvas object
+
 def main():
   canvas = Canvas(API_URL, API_KEY)
   courses = canvas.get_courses()
-  course = canvas.get_course(course_id) #hardcode or sys argv to get course_id
+  #course = canvas.get_course(course_id) #hardcode or sys argv to get course_id
 
-if __name__ == "__main__":
-    main()
+  argSplit = sys.argv[1]
+  course = canvas.get_course(2353385)
+  if(argSplit == 'ungradedassignments'):
+    course =canvas.get_course(sys.argv[2])
+    ungraded_assignments(course)
+  if(argSplit == 'checksub'):
+    assignment = course.get_assignment(sys.argv[3])
+    submission_check(sys.argv[2],assignment)
+  if(argSplit == 'messagequiz'): #TODO:print list of quiz 
+    quiz_message(sys.argv[2],canvas.get_course(sys.argv[3]))
+  if(argSplit == 'quizreport'):
+    quiz_report(sys.argv[2])
+  if(argSplit == 'quizsub'):
+    get_quiz_submissions(sys.argv[2],sys.argv[3])
+  if(argSplit == 'totalnumber'):
+    num_students(canvas.get_course(sys.argv[2]))
+  if(argSplit == 'printassignments'):
+    print_assignments(course)
+  if(argSplit == 'studentids'):
+    studentid(course)
+
 
 def user_info(user_id,course):
   user_by_id = course.get_user(user_id)
   users = course.get_users(search_term='Gaurav Mehta')
   tas = course.get_users(search_term='ta')
 
-def assignment_info(assign_id,course):
-  assn = course.get_assignment(assign_id)
-  ungraded_assn = course.get_assignments(bucket='ungraded')
+def ungraded_assignments(course):
+  ungraded_assn = course.get_assignments(bucket='ungrade')
+  for assignment in ungraded_assn:
+    print(assignment)
 
 def submission_check(user_by_id,assn):
   subs = assn.get_submissions()
@@ -47,25 +70,29 @@ def get_quiz_submissions(quiz,quiz_id):
   quiz_subs = quiz.get_submissions()
   #quiz_sub = quiz.get_submission(6126370)    # insert submission ID
   stats = quiz.get_statistics()
+  print(stats)
 
-def get_students(course):
+def num_students(course):
   studentSize = 0
   users = course.get_users(enrollment_type=['student'])
   for user in users:
     studentSize+=1
 
-def get_teachers(course):
   teacherSize = 0
   users = course.get_users(enrollment_type=['teacher'])
   for user in users:
     teacherSize+=1
 
-def get_tA(course):
-
   taSize = 0
   users = course.get_users(enrollment_type=['ta'])
   for user in users:
     taSize+=1
+
+def studentid(course):
+  studentSize = 0
+  users = course.get_users(enrollment_type=['student'])
+  for user in users:
+    print(user)
 
 #print("Teachers: "+str(teacherSize))
 #print("TAs: "+str(taSize))
@@ -74,9 +101,14 @@ def get_tA(course):
 
 def print_assignments(course):
   assignments = course.get_assignments()
-
+  
   for assignment in assignments:
     print(assignment)
 #/quizlist
 #/assignmentlist
 #/help
+
+
+    
+if __name__ == "__main__":
+  main()
